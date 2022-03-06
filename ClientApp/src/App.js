75ff88f262907d41d5589ac8f34eb7f1961e39e3
hiba.eldursi95@gmail.com <Hiba_Eldursi@next.co.uk>
2022-03-06 00:47:16 +0000

    change class components to functional components

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,17 +1,14 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Route} from 'react-router';
 import {Layout} from './components/Layout';
-import {Home} from './components/Home';
+import Home from './components/Home';
 import './custom.css'
 
-export default class App extends Component {
-    static displayName = App.name;
-
-    render() {
-        return (
-            <Layout>
-                <Route exact path='/' component={Home}/>
-            </Layout>
-        );
-    }
+const App = () => {
+    return (
+        <Layout>
+            <Route exact path='/' component={Home}/>
+        </Layout>
+    );
 }
+export default App
--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -1,47 +1,56 @@
-import React, {Component} from 'react';
+import React, {useState, useEffect} from 'react';
 import './Home.css';
 
-export class Home extends Component {
-    days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-    constructor(props) {
-        super(props);
-        const defaultHeader = this.getDefaultWeather()
-        this.state = {weather: defaultHeader, currentDateTime: new Date()};
-    }
-    static displayName = Home.name;
-
-    getWeather = async(lat, lon) => {
-        const response = await fetch(`weatherforecast?Latitude=${lat}&Longitude=${lon}`);
-        if (response.ok) {
-            const data = await response.json();
-            this.setState({weather: data});
-        } else {
-            console.log(`There is a problem`)
-        }
-    }
-    
-    getDefaultWeather = async () =>{
-        const lat = 51.509865
-        const lon = -0.118092
-        await this.getWeather(lat, lon)
-    }
-    
-    componentDidMount() {
-        this.interval = setInterval(() => this.setState({ currentDateTime: new Date }), 1000);
-        navigator.geolocation.getCurrentPosition((position) => this.getWeather(position.coords.latitude, position.coords.longitude), this.getDefaultWeather );
-    }
-
-    render() {
-        return (
-                <div className={"weather"}>
-                    <h1 className={"weather-header"}>Weather in {this.state.weather.locationName}</h1>
-                    <h3 className={"weather-sub-header"}>{`${this.days[this.state.currentDateTime.getDay()]} - ${new Date().toLocaleString()}`}</h3>
-                    <div className={"weather-details"}>
-                        <img alt="weather-icon" src={`https://openweathermap.org/img/wn/${this.state.weather.icon ?? '04d'}@2x.png`}/>
-                        <span>{this.state.weather.temperature} &deg;C</span> 
-                    </div>
-                </div>
-        );
+const getWeather = async(lat, lon) => {
+    const response = await fetch(`weatherforecast?Latitude=${lat}&Longitude=${lon}`);
+    if (response.ok) {
+        return await response.json();
+    } else {
+        console.log(`There is a problem`)
+        return {}
     }
 }
+
+const IsEmpty = (obj) => {
+    return obj
+        && Object.keys(obj).length === 0
+        && Object.getPrototypeOf(obj) === Object.prototype;
+}
+
+const Home = () => {
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const [weather, setWeather] = useState({});
+    const [currentDateTime, setCurrentDateTime] = useState(new Date());
+    if(IsEmpty(weather))
+    {
+        navigator.geolocation.getCurrentPosition(
+            async (position) => await callback(position.coords.latitude, position.coords.longitude),
+            async () => await callback(51.509865, -0.118092),
+            );
+    }
+    
+    const callback = async (lat, lon) => {
+        const response = await getWeather(lat, lon)
+        if(!IsEmpty(response))
+        {
+            setWeather(response)
+        }
+    }
+
+    useEffect(() => {
+        setInterval(() => setCurrentDateTime(new Date()), 1000);
+    },[])
+    
+    return (
+        <div className={"weather"}>
+            <h1 className={"weather-header"}>Weather in {weather.locationName}</h1>
+            <h3 className={"weather-sub-header"}>{`${days[currentDateTime.getDay()]} - ${currentDateTime.toLocaleString()}`}</h3>
+            <div className={"weather-details"}>
+                <img alt="weather-icon" src={`https://openweathermap.org/img/wn/${weather.icon ?? '04d'}@2x.png`}/>
+                <span>{weather.temperature} &deg;C</span>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
